cd955b7407098d5a5b77cee5abe6eaeb501e7349
David <wipeout4wh@gmail.com>
2013-03-12 20:41:05 -0400

    Added backup functionality

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 import os
+from shutil import copyfile
+import string
 
 class vdf:
+	path = None
+	backupPath = 'files/backups/'
+	backupFile = backupPath + 'sharedconfig.vdf'
 	def fromFile(self):
 		with open('files/data.txt', 'r') as f:
 			vdfPath = f.readline()
@@ -20,23 +25,43 @@ class vdf:
 		userdata = os.listdir('C:/Program Files/Steam/userdata/')
 		if len(userdata) == 1:
 			userID = userdata[0]
-			path = 'C:/Program Files/Steam/userdata/%s/7/remote/sharedconfig.vdf' % userID
-			if os.path.exists(path):
-				return path
+			self.path = 'C:/Program Files/Steam/userdata/%s/7/remote/sharedconfig.vdf' % userID
+			if os.path.exists(self.path):
+				return self.path
 			else:
 				print "Error: Could not find sharedconfig.vdf directory."
 				return 0
 		else:
 			print "Error: Could not find sharedconfig.vdf directory."
 			return 0
-			
 	def getPath(self):
+		if self.path != None:
+			if os.path.exists(self.path):
+				return self.path
 		if self.fromFile() != 0:
 			return self.fromFile()
 		elif self.auto() != 0:
 			return self.auto()
 		else:
 			pass
+	def backup(self):
+		if self.path == None:
+			self.path = self.getPath()
+			if self.path == None:
+				print "Failed to make a backup."
+				return 0
+		if not os.path.exists(self.backupPath):
+			os.makedirs(self.backupPath)
+		if os.path.exists(self.backupFile):
+			print "Backup already exists.",
+			print "Press enter to overwrite, or q to cancel... ",
+			confirm = raw_input()
+			if confirm.lower() == 'q':
+				print "Backup aborted."
+				return 0
+		copyfile(self.path, self.backupFile)
+		print "Successfully backed up sharedconfig.vdf"
+		return 1
 
 def viewCategories():
 	with open('files/categories.txt', 'r') as f:
@@ -49,7 +74,8 @@ def mainMenu():
 	print "1. View Categories"
 	print "2. Edit Categories"
 	print "3. Help"
-	print "4. Quit\n"
+	print "4. Backup Steam sharedconfig file (recommended)"
+	print "5. Quit\n"
 	selection = 0
 	selection = raw_input("What would you like to do? ")
 	print "\n"
@@ -81,8 +107,6 @@ def editCategories():
 	with open(vdf.getPath()) as f:
 		f.write(steamFile)
 	
-
-	
 if __name__ == "__main__":
 	vdf = vdf()
 	while True:
@@ -95,6 +119,8 @@ if __name__ == "__main__":
 			with open('README.md', 'r') as f:
 				print f.read()
 		elif menuSelection == '4':
+			vdf.backup()
+		elif menuSelection == '5':
 			break
 		elif menuSelection == 't':
 			print vdf.getPath()
